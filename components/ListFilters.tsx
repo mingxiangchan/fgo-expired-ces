eadbfeab041bce158f9adfbdfd6d6aefda243a57
Ming Xiang Chan <mingxiangc.isao@gmail.com>
2023-01-21 10:53:40 +0900

    added padding to align things

--- components/ListFilters.tsx
+++ components/ListFilters.tsx
@@ -9,6 +9,7 @@ import {
   setIncludeNonEvent,
   setSorting,
 } from "../utils/reducers/filtersReducer";
+import styles from "../styles/ListFilters.module.css";
 
 export const ListFilters = () => {
   const includeNonEventFilter = useAppSelector(includeNonEvent);
@@ -22,49 +23,55 @@ export const ListFilters = () => {
   }));
 
   return (
-    <Row style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}>
-      <Col offset={1} span={22}>
-        <>
-          <Select
-            defaultValue={SortOptions.atkAsc}
-            options={sortOptions}
-            onChange={(sortOption: SortOptions) =>
-              dispatch(setSorting(sortOption))
-            }
-            style={{ width: 300 }}
-          />
-          <Switch
-            checked={includeNonEventFilter}
-            checkedChildren={"Include Non Event CEs"}
-            unCheckedChildren={"Hide Non Event CEs"}
-            onChange={(option: boolean) => dispatch(setIncludeNonEvent(option))}
-            style={{
-              marginLeft: "1em",
-            }}
-          />
-          {[1, 2, 3, 4, 5].map((rarityOpt) => {
-            const included = includedRarities.indexOf(rarityOpt) !== -1;
+    <Row
+      style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}
+      justify="start"
+      align="middle"
+    >
+      <Col offset={1} xs={22} lg={5} className={styles.input}>
+        <Select
+          defaultValue={SortOptions.atkAsc}
+          options={sortOptions}
+          onChange={(sortOption: SortOptions) =>
+            dispatch(setSorting(sortOption))
+          }
+          style={{ width: 300 }}
+        />
+      </Col>
+      <Col xs={22} lg={3} className={styles.input}>
+        <Switch
+          checked={includeNonEventFilter}
+          checkedChildren={"Include Non Event CEs"}
+          unCheckedChildren={"Hide Non Event CEs"}
+          onChange={(option: boolean) => dispatch(setIncludeNonEvent(option))}
+          style={{
+            marginLeft: "1em",
+          }}
+        />
+      </Col>
+      <Col xs={22} lg={6} className={styles.input}>
+        {[1, 2, 3, 4, 5].map((rarityOpt) => {
+          const included = includedRarities.indexOf(rarityOpt) !== -1;
 
-            return (
-              <Switch
-                key={rarityOpt}
-                checked={included}
-                checkedChildren={`${rarityOpt} Star`}
-                unCheckedChildren={`${rarityOpt} Star`}
-                onChange={(option: boolean) => {
-                  if (option === true) {
-                    dispatch(includeRarity(rarityOpt));
-                  } else {
-                    dispatch(excludeRarity(rarityOpt));
-                  }
-                }}
-                style={{
-                  marginLeft: "1em",
-                }}
-              />
-            );
-          })}
-        </>
+          return (
+            <Switch
+              key={rarityOpt}
+              checked={included}
+              checkedChildren={`${rarityOpt}☆`}
+              unCheckedChildren={`${rarityOpt}☆`}
+              onChange={(option: boolean) => {
+                if (option === true) {
+                  dispatch(includeRarity(rarityOpt));
+                } else {
+                  dispatch(excludeRarity(rarityOpt));
+                }
+              }}
+              style={{
+                marginLeft: "1em",
+              }}
+            />
+          );
+        })}
       </Col>
     </Row>
   );
